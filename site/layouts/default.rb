a95b90b6a3ce0f4c7e20638675609aa95c73a80a
Charles B Johnson <mail@cbjohnson.info>
2021-11-14 22:45:46 -0600

    refactor: move default layout content into main slot

--- site/layouts/default.rb
+++ site/layouts/default.rb
@@ -2,15 +2,22 @@
 
 module Layouts
   class Default < Layout
-    def initialize(page:, **opts)
+    def initialize(page:, **opts, &block)
       super(page: page)
 
       self.favicon = opts[:favicon]
       self.description = opts[:description]
+
+      block&.call(self)
+    end
+
+    def main
+      block_given? ? self.main = yield : @main
     end
 
     private
 
     attr_accessor :favicon, :description
+    attr_writer :main
   end
 end
